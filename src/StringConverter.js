b1033260e55da6cb7782f553adb56a9e3e4baf14
Sebastian <sa224ny@student.lnu.se>
2021-09-03 15:17:08 +0200

    Add ability to log out name

--- src/StringConverter.js
+++ src/StringConverter.js
@@ -14,3 +14,5 @@ class StringConverter {
   }
 
 }
+
+export default StringConverter
--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import StringConverter from './StringConverter.js'
 
+import readline from 'readline'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const stringConverter = new StringConverter()
+
+rl.question('What\'s your name?\n', (answer) => {
+  console.log(`Welcome ${answer}!`)
+  stringConverter.setName(answer)
+  rl.close()
+})
